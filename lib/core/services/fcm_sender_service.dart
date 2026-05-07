2a32d736db820b9d766b409f05a9b9829e24623a
Howaida <hieda774900@gmail.com>
2026-05-07 07:54:04 +0300

    mmm

--- lib/core/services/fcm_sender_service.dart
+++ lib/core/services/fcm_sender_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/services.dart';
 import 'package:googleapis_auth/auth_io.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 class FcmSenderService {
   static const String _projectId = 'carwash-go-a8adc';
